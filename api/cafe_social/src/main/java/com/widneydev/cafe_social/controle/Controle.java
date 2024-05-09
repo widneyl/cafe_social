bd7353ff540a596f16067533bef79738aff07b18
Widney Lima <widneylima21@gmail.com>
2024-05-09 13:11:05 -0300

    Adicionando as pastas do front end

---
+++ api/cafe_social/src/main/java/com/widneydev/cafe_social/controle/Controle.java
@@ -0,0 +1,6 @@
+package com.widneydev.cafe_social.controle;
+
+//Aqui estara os metodos de comuniccação com o banco de dados
+public class Controle {
+    
+}
---
+++ api/cafe_social/src/main/java/com/widneydev/cafe_social/repositorio/Repositorio.java
@@ -0,0 +1,7 @@
+package com.widneydev.cafe_social.repositorio;
+
+//Aqui estarão as Rotas
+
+public class Repositorio {
+    
+}
